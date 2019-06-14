a38874f1fec6837803e93ea56423482807cfa884
JoeBew42 <joebew42@gmail.com>
2019-06-14 18:55:50 +0200

    green bar: introduce the concept of rolls

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,14 +1,15 @@
 class BowlingGame {
   constructor() {
-    this._score = 0;
+    this._currentRoll = 0;
+    this._rolls = Array(21).fill(0)
   }
 
   roll(pins) {
-    this._score += pins;
+    this._rolls[this._currentRoll++] = pins
   }
 
   score() {
-    return this._score;
+    return this._rolls.reduce((acc, curr) => { return acc + curr });
   }
 }
 
