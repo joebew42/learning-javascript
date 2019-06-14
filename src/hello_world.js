8b22450c3d2248be592f284e1994d71fef714176
JoeBew42 <joebew42@gmail.com>
2019-06-14 15:10:09 +0200

    a class

--- src/hello_world.js
+++ src/hello_world.js
@@ -1,5 +1,7 @@
-module.exports = {
-  sayHello: () => {
+class HelloWorld {
+  sayHello() {
     return "Hello World"
   }
-};
+}
+
+module.exports = HelloWorld;
--- test/hello_world.test.js
+++ test/hello_world.test.js
@@ -1,9 +1,12 @@
 var expect = require('chai').expect;
-var helloWorld = require('../src/hello_world');
+
+var HelloWorld = require('../src/hello_world');
 
 describe("HelloWorld", () => {
   describe("sayHello", () => {
     it("return Hello World", () => {
+      helloWorld = new HelloWorld();
+
       expect(helloWorld.sayHello()).to.equal("Hello World");
     });
   });
