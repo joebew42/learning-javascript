439f910d0e2312037245ac8e1399bd9d5e8e63a2
JoeBew42 <joebew42@gmail.com>
2019-06-14 17:47:55 +0200

    green bar: better naming LOL

--- test/BowlingGameTest.js
+++ test/BowlingGameTest.js
@@ -2,14 +2,18 @@ var assert = require('chai').assert
 
 import BowlingGame from '../src/BowlingGame'
 
-it("xxx", () => {
-  var game = new BowlingGame();
 
-  var i;
-  for (i = 0; i < 20; i++) {
-    game.roll(0);
-  }
+describe("BowlingGame", () => {
+  it("gutter game", () => {
+    var game = new BowlingGame();
 
-  assert.equal(game.score(), 0);
+    var i;
+    for (i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+
+    assert.equal(game.score(), 0);
+  });
 });
+
 
