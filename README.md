0909fc492745cae0a1d9d413bdaed490835c1db0
JoeBew42 <joebew42@gmail.com>
2019-06-14 13:43:24 +0200

    assert true equal true :)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning JavaScript
+
+My Journey in trying to learn something about JavaScript
+
+```
+npm install
+npm test
+```
---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var helloWorld = require('./index');
+
+describe("printHelloWorld", () => {
+  it("should return Hello World", () => {
+    expect(true).to.be.true;
+  });
+});
