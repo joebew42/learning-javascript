e2145845a5bdf7c12784963618dd7eb116ab3ae0
JoeBew42 <joebew42@gmail.com>
2019-06-16 21:35:43 +0200

    RomanNumerals - greenbar - removed the duplication

--- test/RomanNumeralsTest.js
+++ test/RomanNumeralsTest.js
@@ -9,43 +9,22 @@ describe("RomanNumerals", () => {
     romanNumerals = RomanNumerals.new();
   })
 
-  it("converts 1 to I", () => {
-    assert.equal(romanNumerals.convert(1), "I");
-  })
+  var romans = {
+    1:  "I",
+    2:  "II",
+    3:  "III",
+    4:  "IV",
+    5:  "V",
+    6:  "VI",
+    7:  "VII",
+    8:  "VIII",
+    9:  "IX",
+    10: "X"
+  }
 
-  it("converts 2 to II", () => {
-    assert.equal(romanNumerals.convert(2), "II");
-  })
-
-  it("converts 3 to III", () => {
-    assert.equal(romanNumerals.convert(3), "III");
-  })
-
-  it("converts 4 to IV", () => {
-    assert.equal(romanNumerals.convert(4), "IV");
-  })
-
-  it("converts 5 to V", () => {
-    assert.equal(romanNumerals.convert(5), "V");
-  })
-
-  it("converts 6 to VI", () => {
-    assert.equal(romanNumerals.convert(6), "VI");
-  })
-
-  it("converts 7 to VII", () => {
-    assert.equal(romanNumerals.convert(7), "VII");
-  })
-
-  it("converts 8 to VIII", () => {
-    assert.equal(romanNumerals.convert(8), "VIII");
-  })
-
-  it("converts 9 to IX", () => {
-    assert.equal(romanNumerals.convert(9), "IX");
-  })
-
-  it("converts 10 to X", () => {
-    assert.equal(romanNumerals.convert(10), "X");
-  })
+  for (var number in romans) {
+    it(`converts ${number} to ${romans[number]}`, () => {
+      assert.equal(romanNumerals.convert(number), romans[number]);
+    })
+  }
 });
