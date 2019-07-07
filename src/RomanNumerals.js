79d1081d161057b77694c2a86c8abf8f93165f6e
JoeBew42 <joebew42@gmail.com>
2019-07-07 15:35:09 +0200

    RomanNumerals - green - starting from the beginning

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,63 +1,6 @@
 const _RomanNumerals = {
   convert: function(number) {
-    if (number == 100) {
-      return "C";
-    }
-    return this._convertUpTo99(number);
-  },
-
-  _convertUpTo99: function(number) {
-    var rest = number % 90;
-
-    return "XC" + this._convertUpTo89(rest);
-  },
-
-  _convertUpTo89: function(number) {
-    var rest = number % 50;
-
-    return "L" + this._convertUpTo49(rest);
-  },
-
-  _convertUpTo49: function(number) {
-    var times = Math.floor(number / 10);
-    var rest = number % 10;
-
-    if (times < 4) {
-      return "X".repeat(times) + this._convertUpTo10(rest);
-    } else {
-      return "XL" + this._convertUpTo10(rest);
-    }
-  },
-
-  _convertUpTo10: function(number) {
-    if (number == 9) {
-      return "IX";
-    }
-    if (number == 8) {
-      return "VIII";
-    }
-    if (number == 7) {
-      return "VII";
-    }
-    if (number == 6) {
-      return "VI";
-    }
-    if (number == 5) {
-      return "V";
-    }
-    if (number == 4) {
-      return "IV";
-    }
-    if (number == 3) {
-      return "III";
-    }
-    if (number == 2) {
-      return "II";
-    }
-    if (number == 1) {
-      return "I";
-    }
-    return "";
+    return "I";
   }
 }
 
--- test/RomanNumeralsTest.js
+++ test/RomanNumeralsTest.js
@@ -9,42 +9,43 @@ describe("RomanNumerals", () => {
     romanNumerals = RomanNumerals.new();
   })
 
-  var romans = {
-    1:    "I",
-    2:    "II",
-    3:    "III",
-    4:    "IV",
-    5:    "V",
-    6:    "VI",
-    7:    "VII",
-    8:    "VIII",
-    9:    "IX",
-    10:   "X",
-    11:   "XI",
-    12:   "XII",
-    15:   "XV",
-    19:   "XIX",
-    20:   "XX",
-    30:   "XXX",
-    49:   "XLIX",
-    50:   "L",
-    60:   "LX",
-    70:   "LXX",
-    79:   "LXXIX",
-    80:   "LXXX",
-    89:   "LXXXIX",
-    90:   "XC",
-    91:   "XCI",
-    92:   "XCII",
-    99:   "XCIX",
-    100:  "C",
-    // 500:  "D",
-    // 1000: "M",
-  }
+  var romans =
+    Object.entries({
+      1:    "I",
+      // 2:    "II",
+      // 3:    "III",
+      // 4:    "IV",
+      // 5:    "V",
+      // 6:    "VI",
+      // 7:    "VII",
+      // 8:    "VIII",
+      // 9:    "IX",
+      // 10:   "X",
+      // 11:   "XI",
+      // 12:   "XII",
+      // 15:   "XV",
+      // 19:   "XIX",
+      // 20:   "XX",
+      // 30:   "XXX",
+      // 49:   "XLIX",
+      // 50:   "L",
+      // 60:   "LX",
+      // 70:   "LXX",
+      // 79:   "LXXIX",
+      // 80:   "LXXX",
+      // 89:   "LXXXIX",
+      // 90:   "XC",
+      // 91:   "XCI",
+      // 92:   "XCII",
+      // 99:   "XCIX",
+      // 100:  "C",
+      // 500:  "D",
+      // 1000: "M",
+    });
 
-  for (var number in romans) {
-    it(`converts ${number}\tto ${romans[number]}`, () => {
-      assert.equal(romanNumerals.convert(number), romans[number]);
+  romans.forEach(([decimal, roman]) => {
+    it(`converts ${decimal}\tto ${roman}`, () => {
+      assert.equal(romanNumerals.convert(decimal), roman);
     })
-  }
+  })
 });
