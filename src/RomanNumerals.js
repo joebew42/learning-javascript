453c452ca8c81f77bfd46c13f8ca8323fcded22e
JoeBew42 <joebew42@gmail.com>
2019-07-07 15:50:48 +0200

    RomanNumerals - green - extract convertUnits

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,5 +1,26 @@
 const _RomanNumerals = {
   convert: function(number) {
+    if (number == 19) {
+      return "XIX";
+    }
+    if (number == 18) {
+      return "XVIII";
+    }
+    if (number == 16) {
+      return "XVI";
+    }
+    if (number == 15) {
+      return "XV";
+    }
+    if (number == 14) {
+      return "XIV";
+    }
+    if (number == 12) {
+      return "XII";
+    }
+    if (number == 11) {
+      return "XI";
+    }
     if (number == 10) {
       return "X";
     }
@@ -12,6 +33,10 @@ const _RomanNumerals = {
     if (number == 4) {
       return "IV";
     }
+    return this._convertUnits(number);
+  },
+
+  _convertUnits: function(number) {
     return "I".repeat(number);
   }
 }
