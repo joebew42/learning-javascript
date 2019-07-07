9448fe9d1bd50b45fedfa311d3be1c74ab35f7f9
JoeBew42 <joebew42@gmail.com>
2019-07-07 15:39:41 +0200

    RomanNumerals - green - convert til 10

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,5 +1,32 @@
 const _RomanNumerals = {
   convert: function(number) {
+    if (number == 10) {
+      return "X";
+    }
+    if (number == 9) {
+      return "IX";
+    }
+    if (number == 8) {
+      return "VIII";
+    }
+    if (number == 7) {
+      return "VII";
+    }
+    if (number == 6) {
+      return "VI";
+    }
+    if (number == 5) {
+      return "V";
+    }
+    if (number == 4) {
+      return "IV";
+    }
+    if (number == 3) {
+      return "III";
+    }
+    if (number == 2) {
+      return "II";
+    }
     return "I";
   }
 }
