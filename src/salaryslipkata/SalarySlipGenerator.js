eafb9471ad3ba282ce065e85fdb32879dba2d681
Joe Bew <joebew42@gmail.com>
2021-05-31 19:59:59 +0200

    Move #monthlyGrossSalary to SalarySlipGenerator#generateFor

--- src/salaryslipkata/SalarySlipGenerator.js
+++ src/salaryslipkata/SalarySlipGenerator.js
@@ -1,6 +1,15 @@
 class SalarySlipGenerator {
-  constructor() {
+  constructor() {}
+
+  generateFor(annualGrossSalary) {
+    return this.#roundUp(annualGrossSalary / 12, 2);
+  }
+
+  #roundUp(number, decimalDigits) {
+    let factor = Math.pow(10, decimalDigits);
+    return Math.round(number * factor) / factor;
   }
 }
 
 export default SalarySlipGenerator;
+
--- test/salaryslipkata/SalarySlipGeneratorTest.js
+++ test/salaryslipkata/SalarySlipGeneratorTest.js
@@ -1,13 +1,16 @@
 import { assert } from "chai";
+import SalarySlipGenerator from "../../src/salaryslipkata/SalarySlipGenerator";
 
 describe("SalarySlipGenerator", () => {
   beforeEach(() => {});
 
   it("calculates the monthly gross salary from the annual one", () => {
+    let salarySlip = new SalarySlipGenerator();
+
     let annualGrossSalary = 5000;
     let monthlyGrossSalary = 416.67;
 
-    assert.equal(monthlyGrossSalaryFrom(annualGrossSalary), monthlyGrossSalary);
+    assert.equal(salarySlip.generateFor(annualGrossSalary), monthlyGrossSalary);
   });
 
   it("rounds a number up to 0 decimal digits", () => {
@@ -28,10 +31,6 @@ describe("SalarySlipGenerator", () => {
     assert.equal(roundUp(number, 2), 416.67);
   });
 });
-
-function monthlyGrossSalaryFrom(annualGrossSalary) {
-  return roundUp(annualGrossSalary / 12, 2);
-}
 
 function roundUp(number, decimalDigits) {
   let factor = Math.pow(10, decimalDigits);
