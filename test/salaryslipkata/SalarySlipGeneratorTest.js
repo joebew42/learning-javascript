b518cf9471853a36c82d30321e4086c517681045
Joe Bew <joebew42@gmail.com>
2021-05-31 20:02:03 +0200

    Remove uneeded tests for #roundUp

--- test/salaryslipkata/SalarySlipGeneratorTest.js
+++ test/salaryslipkata/SalarySlipGeneratorTest.js
@@ -12,28 +12,4 @@ describe("SalarySlipGenerator", () => {
 
     assert.equal(salarySlip.generateFor(annualGrossSalary), monthlyGrossSalary);
   });
-
-  it("rounds a number up to 0 decimal digits", () => {
-    let number = 416.66667;
-
-    assert.equal(roundUp(number, 0), 417);
-  });
-
-  it("rounds a number up to 1 decimal digit", () => {
-    let number = 416.66667;
-
-    assert.equal(roundUp(number, 1), 416.7);
-  });
-
-  it("rounds a number up to 2 decimal digits", () => {
-    let number = 416.66667;
-
-    assert.equal(roundUp(number, 2), 416.67);
-  });
 });
-
-function roundUp(number, decimalDigits) {
-  let factor = Math.pow(10, decimalDigits);
-
-  return Math.round(number * factor) / factor;
-}
